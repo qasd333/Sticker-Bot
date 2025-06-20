448d8a113b75d7540ab574219ba8c2af9b57178c
ilya <160175206+qasd333@users.noreply.github.com>
2025-06-20 10:32:40 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -2,7 +2,7 @@ API_ID = 1488 # получить с https://my.telegram.org/
 API_HASH = '1488' # получить с https://my.telegram.org/
 
 COUNT_FOR_BUY_STARS = 1 # сколько стикеров бот купит за старсы
-COUNT_FOR_BUY_TON = 1 # сколько стикеров бот купит за тон
+COUNT_FOR_BUY_TON = 1 # сколько стикеров бот купит за тон, 0 если не нужно
 
 mnemonic = ['word1', 'word2', 'word3']
 
