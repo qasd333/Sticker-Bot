bdfffd267f164fae16ca2fe108b10a1cb6a63170
ilya <160175206+qasd333@users.noreply.github.com>
2025-06-20 03:51:16 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,6 +1,9 @@
 API_ID = 1488 # получить с https://my.telegram.org/
 API_HASH = '1488' # получить с https://my.telegram.org/
 
-COUNT_FOR_BUY = 3 # сколько стикеров бот купит
+COUNT_FOR_BUY_STARS = 1 # сколько стикеров бот купит за старсы
+COUNT_FOR_BUY_TON = 1 # сколько стикеров бот купит за тон
+
+mnemonic = ['word1', 'word2', 'word3']
 
 STICKER_NAME = 'Bored' # название новых стикеров
